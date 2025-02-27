74f49826baa38828e96ebab96039157023a95f47
jameslester78 <58671573+jameslester78@users.noreply.github.com>
2025-02-27 13:26:35 +0000

    basic working with workflow params

--- python/py_basics.py
+++ python/py_basics.py
@@ -10,7 +10,28 @@ print(list1)
 
 enumerate(list1) #add an index to each member of a list 0,1,2...
 
-
 #basics
 range(100) #produce a sequence 0-99
 
+num1, num2,  num3 = (1,2,3) #unpack tuple to variables
+
+global team #global variable
+
+def echo(n): #a variable can hold a function
+    def inner_echo(word1):
+        echo_word = word1 * n
+        return echo_word
+    return inner_echo
+twice = echo(2)
+print(twice('hello')) 
+
+#an inner function can access variables from the enclosing function with the nonlocal keyword
+def echo_shout(word):
+    echo_word = word*2
+    print(echo_word)
+    def shout():
+        nonlocal echo_word
+        echo_word = echo_word + '!!!'
+    shout()
+    print(echo_word)
+echo_shout('hello')
